f9a233d086bcb13497ea9c3d246b27c757618dac
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-03-11 19:29:41 -0400

    last of chapter21

---
+++ Bullseye/HighScoreItem.swift
@@ -0,0 +1,13 @@
+//
+//  HighScoreItem.swift
+//  Bullseye
+//
+//  Created by Louis Ramirez on 3/11/20.
+//  Copyright © 2020 Louis Ramirez. All rights reserved.
+//
+
+import Foundation
+class HighScoreItem {
+  var name = ""
+  var score = 0    
+}
